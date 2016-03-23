add31a1992228e12c62407119949cefb169f2070
Duncan McGreggor <oubiwann@gmail.com>
2016-03-23 13:38:33 -0500

    Formatting.

--- dev/install-packages.r
+++ dev/install-packages.r
@@ -1,5 +1,7 @@
 if("devtools" %in% rownames(installed.packages()) == FALSE) {
-    install.packages(c("devtools"), Sys.getenv("R_LIBS_USER"), repos='http://cran.us.r-project.org')
+    install.packages(c("devtools"),
+                     Sys.getenv("R_LIBS_USER"),
+                     repos='http://cran.us.r-project.org')
 }
 library("devtools")
 devtools::install_github("oubiwann/lcmap-client-r")
