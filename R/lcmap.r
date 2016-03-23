311d3511e99bde77becfa94e82ed8dd553849c3d
Duncan McGreggor <oubiwann@gmail.com>
2016-03-23 15:07:10 -0500

    Added version and routes.

--- R/lcmap.r
+++ R/lcmap.r
@@ -2,3 +2,16 @@
 #'
 #' @name lcmap
 #' @docType package
+clientVersion<-0.0.1
+
+routes<-hash::hash(baseContext="/api",
+                   authContext="/api/auth",
+                   loginContext="/api/auth/login",
+                   userContext="/api/auth/me",
+                   compatContext="/api/compatibility",
+                   dataContext="/api/data",
+                   jobsContext="/api/jobs",
+                   modelsContext="/api/models",
+                   eventsContext="/api/events",
+                   systemContext="/api/system",
+                   usersContext="/api/users")
