19233e7a3f4ee17b083055566379785b5882557f
Duncan McGreggor <oubiwann@gmail.com>
2016-03-23 13:37:25 -0500

    Only install devtools if it isn't.

--- dev/install-packages.r
+++ dev/install-packages.r
@@ -1,3 +1,5 @@
-install.packages(c("devtools"), Sys.getenv("R_LIBS_USER"), repos='http://cran.us.r-project.org')
+if("devtools" %in% rownames(installed.packages()) == FALSE) {
+    install.packages(c("devtools"), Sys.getenv("R_LIBS_USER"), repos='http://cran.us.r-project.org')
+}
 library("devtools")
 devtools::install_github("oubiwann/lcmap-client-r")
