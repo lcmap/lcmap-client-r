6b82ebcabf4337a7592e639267e42bfe3b69b5d5
Duncan McGreggor <oubiwann@gmail.com>
2016-03-23 15:07:40 -0500

    First step towards http wrapper.

---
+++ R/http.r
@@ -0,0 +1,52 @@
+defaultEndpoint = "http://"
+clientVendor = "vnd.usgs.lcmap"
+projectUrl = "https://github.com/oubiwann/lcmap-client-r"
+mediaTypePrefix = "application"
+mediaTypeSuffix = "json"
+
+#' Format the client user agent string
+#'
+#' @export
+#' @examples
+#' formatUserAgent()
+formatUserAgent <- function() {
+    userAgent<-sprintf("LCMAP REST Client/%s (%s) (+%s)",
+        lcmap::clientVersion, version$version.string, projectUrl)
+    return(userAgent)
+}
+
+#' Format the data for the 'Accept' header
+#'
+#' @param vendor vendor
+#' @param version version
+#' @param contentType contentType
+#' @export
+#' @examples
+#' formatAccept("vnd.some.one", "2.0", "text/xml")
+formatAccept <- function(vendor, version, contentType) {
+    mediaType<-strsplit(contentType, "/")[[1]]
+    accept<-sprintf("%s/%s.v%s+%s", mediaType[1], vendor, version, mediaType[2])
+    return(accept)
+}
+
+request <- function(method, path, ...) {
+    func<-sprintf("httr::%s", method)
+    url<-sprintf("%s%s", getCfg$endpoint, path)
+    do.call(func, list(url, ...))
+}
+
+get <- function(path, ...) {
+    request("GET", path, ...)
+}
+
+post <- function(path, ...) {
+    request("POST", path, ...)
+}
+
+put <- function(path, ...) {
+    request("PUT", path, ...)
+}
+
+delete <- function(path, ...) {
+    request("DELETE", path, ...)
+}
